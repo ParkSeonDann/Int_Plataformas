58c762d7eb56625a70fc3af1bd8c220db051fd1e
Faflo <fa.flores@duocuc.cl>
2024-05-28 22:44:35 -0400

    crud voy en la c :)

---
+++ soporte/soporte/functions-for-firebase.js
@@ -0,0 +1,13 @@
+// functions-for-firebase.js
+
+function guardar() {
+    try {
+        const docRef = firebase.firestore().collection("usuarios").add({
+            nombre: document.getElementById("name").value,
+            apellido: document.getElementById("last").value,
+        });
+        alert("congrat, registro exitoso.");
+    } catch (e) {
+        alert("Error en el registro ", e);
+    }
+}
